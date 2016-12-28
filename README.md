4302e9f6ef9f0217b4e61f34db5b824c1dbc656b
Thomas Fuller <thomas@mcfuller.com>
2016-12-27 22:39:03 -0800

    Move problem test function into a separate file.

---
+++ README.md
@@ -0,0 +1,3 @@
+This fork only removes the reference to the A0 pin in a test method to get the 
+library working with the RFduino
+
---
+++ RunningStatisticsTests.cpp
@@ -0,0 +1,31 @@
+#include"RunningStatistics.cpp"
+
+void testRunningStatistics() {
+  // a speed test for running statistics
+  
+  RunningStatistics myStats;
+  
+  myStats.setInitialValue( (1.0/1024)*float(analogRead( A0 )) );
+  
+  float updateInterval = 1.0;
+  float nextUpdateTime = 1e-6*float(micros()) + updateInterval;
+  
+  long nLoops = 0;
+  
+  while( true ) {
+    myStats.input( (1.0/1024)*float(analogRead( A0 )) );
+    nLoops++;
+    float t = 1e-6*float(micros());
+    
+    if( t > nextUpdateTime ) {
+      nextUpdateTime += updateInterval;
+      
+      Serial.print( "mean: "); Serial.print( myStats.mean() );
+      Serial.print( "\tsigma: " ); Serial.print( myStats.sigma() );
+      Serial.print( "\tHz: "); Serial.print( nLoops );
+      
+      nLoops = 0;
+      Serial.print("\n");
+    }
+  }
+}
